fc86777e75d13d4984d221ab12724642d8eefdb9
drakeira <thiago.p.dasilva2005@gmail.com>
2022-09-09 17:55:54 -0300

    feat: add task item component

--- src/components/TaskItem/index.tsx
+++ src/components/TaskItem/index.tsx
@@ -1,14 +1,18 @@
-import { ITask } from "../../interfaces/models/ITask"
-import { Container } from "./styles"
+import { ITask } from "../../interfaces/models/ITask";
+import { Container } from "./styles";
+import { AiFillCheckCircle } from "react-icons/ai";
 
 interface TaskItemProps {
-    task: ITask;
+  task: ITask;
 }
 
-export const TaskItem = ({task}: TaskItemProps) => {
-    return (
-        <Container>
-            {task.title}
-        </Container>
-    )
-}
+export const TaskItem = ({ task }: TaskItemProps) => {
+  return (
+    <Container>
+      {task.title}
+      <div className="concluded-container">
+        <AiFillCheckCircle />
+      </div>
+    </Container>
+  );
+};
--- src/components/TaskItem/styles.ts
+++ src/components/TaskItem/styles.ts
@@ -4,10 +4,25 @@ export const Container = styled.div`
     width: 100%;
     height: 60px;
 
+    padding: 40px;
+
     border-bottom: 2px solid rgba(0,0,0,0.1);
     border-radius: 4px;
 
     display: flex;
-    justify-content: space-around;
+    justify-content: space-between;
     align-items: center;
+
+    .concluded-container {
+        color: #14870c;
+        font-size: large;
+
+        cursor: pointer;
+
+        text-align: center;
+
+        &:hover {
+            filter: brightness(0.9);
+        }
+    }
 `
