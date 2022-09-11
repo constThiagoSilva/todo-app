e6065621d38ea9509b015605ed5a2ab4124d551d
drakeira <thiago.p.dasilva2005@gmail.com>
2022-09-11 12:29:14 -0300

    feat: add axios interceptors

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,9 +1,26 @@
-import axios, { HeadersDefaults } from 'axios'
+import axios, { AxiosRequestHeaders, HeadersDefaults } from 'axios'
 
 export interface CommomHeaderProperties extends HeadersDefaults {
     authorization: string;
 }
 
+export interface CommonHeaderInteceptors extends AxiosRequestHeaders {
+    authorization: string;
+}
+
 export const api = axios.create({
     baseURL: 'http://localhost:4444',
+})
+
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem('token')
+
+    if (token) {
+        config.headers = {authorization: `Bearer ${token}`} as CommonHeaderInteceptors
+
+        return config
+    }
+
+}, (error) => {
+    return Promise.reject(error)
 })
